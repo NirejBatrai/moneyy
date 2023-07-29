01ebc5e0f4f6eca6a61a8e1ca47a499705c20b9e
Nirej <purampandey1234@gmail.com>
2023-07-29 14:00:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// // ways to declear a varible
+// //var, let , const
+
+// // let firstName = "jhon";
+// // let lastName = "doe";
+// // console.log(firstName, lastName);
+// // let age = 30;
+// // console.log(age);
+// // const person = "Nil uzuma";
+// // person.email = "niluzum@gmail.com";
+// // console.log(person);
+// //string
+// const firstName = "sara";
+
+// //number
+
+// const age = 30;
+// const temp = 98.3;
+
+// //boolean
+// const hasKids = true;
+// Null
+// const aptNumber = "null";
+// undefine
+// let score;
+// const score=undefined;
+// symbol
+// const id=Symbol('id');
+// bigIn
+// const n=900
+// const output = id;
+// console.log(output, typeof output);
+
+// const student_id = {
+//   "roll no": 100,
+//   " name": "code qith Nil",
+//   "status    ": true,
+// };
+// console.log(student_id["roll no"]);
+// const worker_id = {
+//   " name": "shell",
+//   "age  ": 26,
+//   " gender": "male",
+//   "status  ": true,
+// };
+// console.log(worker_id);
+// const student_id = {
+//   "name  ": "nil is hero",
+//   "roll no": 100,
+//   " status ": true,
+// };
+// console.log(student_id);
